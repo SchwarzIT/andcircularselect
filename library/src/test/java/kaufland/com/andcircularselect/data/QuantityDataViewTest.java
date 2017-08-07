c80dd3f797c32aad6f4015d17168c4610f6630d3
Sven Braune <sven.braune@kaufland.com>
2017-08-07 18:23:49 +0200

    # more tests

--- library/src/test/java/kaufland/com/andcircularselect/data/QuantityDataViewTest.java
+++ library/src/test/java/kaufland/com/andcircularselect/data/QuantityDataViewTest.java
@@ -1,14 +1,34 @@
 package kaufland.com.andcircularselect.data;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import java.util.List;
+
+import kaufland.com.andcircularselect.BuildConfig;
 import kaufland.com.andcircularselect.ReflectionUtil;
+import kaufland.com.andcircularselect.renderer.CircularRenderer;
+
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by sbra0902 on 04.08.17.
  */
-
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class QuantityDataViewTest {
 
 
@@ -30,6 +50,43 @@ public class QuantityDataViewTest {
         Assert.assertEquals(7, ReflectionUtil.fieldGet(QuantityDataView.class, view, "smallLineCount"));
         Assert.assertEquals(8, ReflectionUtil.fieldGet(QuantityDataView.class, view, "smallSelectorColor"));
 
+    }
+
+    @Test
+    public void testDrawControl() {
+
+        Context contextMock = Mockito.mock(Context.class);
+        Resources resMock = Mockito.mock(Resources.class);
+        Mockito.when(contextMock.getResources()).thenReturn(resMock);
+        Mockito.when(resMock.getDimension(anyInt())).thenReturn(2f);
+
+        Canvas canvasMock = Mockito.mock(Canvas.class);
+        RectF rect = new RectF(0, 0, 150, 150);
+
+        QuantityDataView view = new QuantityDataView.Builder()
+                .withSmallLineCount(3)
+                .withSmallSelectorColor(2)
+                .withSelectorWidth(2)
+                .withSelectorColor(5)
+                .withQuantity(3)
+                .withTextSize(1)
+                .build();
+
+        view.draw(contextMock, canvasMock, rect, 90, 90);
+
+        final ArgumentCaptor<Paint> captor = ArgumentCaptor.forClass(Paint.class);
+        final ArgumentCaptor<RectF> captorRect = ArgumentCaptor.forClass(RectF.class);
+        final ArgumentCaptor<String> captorString = ArgumentCaptor.forClass(String.class);
+        final ArgumentCaptor<Integer> captorX = ArgumentCaptor.forClass(Integer.class);
+        final ArgumentCaptor<Integer> captorY = ArgumentCaptor.forClass(Integer.class);
+
+        verify(canvasMock).drawText(captorString.capture(), captorX.capture(), captorY.capture(), captor.capture());
+
+        List<String> allQuantities = captorString.getAllValues();
+        Assert.assertEquals(allQuantities.get(0), "3");
+
+        Assert.assertEquals(captor.getValue().getColor(), 2);
+        Assert.assertEquals(captor.getValue().getTextSize(), 1, 0);
     }
 
 }
